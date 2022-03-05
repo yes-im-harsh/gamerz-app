ab3acaeec91430e4fba22c9ce27f674cbb052731
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-03-05 17:37:09 +0530

    Dependencies added!

--- src/App.js
+++ src/App.js
@@ -2,17 +2,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <button className="btn btn-ghost">Hello</button>
       </header>
     </div>
   );
